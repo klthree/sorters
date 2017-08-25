34ee5959c3fc0b94ec6231f43d6f812af00d395d
Michael Buckner <bucknerml@gmail.com>
2017-08-25 23:19:32 +0000

    change parameters merge

--- merge.c
+++ merge.c
@@ -4,10 +4,15 @@
 
 void sort (int array[], int start, int end)
 {
+    int mid = (start + end) / 2;
 
+    sort (array, start, mid);
+    sort (array, mid + 1, end);
+
+    merge(array, start, mid, mid + 1, end);
 }
 
-void merge (int array[], int size)
+void merge (int array[], int low, mid1, mid2, end)
 {
 
 }
