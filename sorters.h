53acbd8393c24630298c364f79fbdfb4154ff442
Michael Buckner <bucknerml@gmail.com>
2017-08-31 01:39:12 +0000

    header file

---
+++ sorters.h
@@ -0,0 +1,24 @@
+/**
+ * Sorting function headers
+ */
+
+#include "sorters.h"
+
+
+/**
+ * Merge sort
+ */
+void sort (int array[], int start, int end);
+void merge (int array[], int size, int low, int mid1, int mid2, int end);
+
+/**
+ * Insertion sort
+ */
+
+/**
+ * Selection sort
+ */
+
+/**
+ * Bubble sort
+ */
