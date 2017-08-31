d7a916ecd288945d3a0eb0e7885e2f77412b6c62
Michael Buckner <bucknerml@gmail.com>
2017-08-31 01:31:33 +0000

    progress on merge

--- merge.c
+++ merge.c
@@ -5,14 +5,51 @@
 void sort (int array[], int start, int end)
 {
     int mid = (start + end) / 2;
+    int size = end - start;
 
     sort (array, start, mid);
     sort (array, mid + 1, end);
 
-    merge(array, start, mid, mid + 1, end);
+    merge(array, size, start, mid, mid + 1, end);
 }
 
-void merge (int array[], int low, int mid1, int mid2, int end)
+void merge (int array[], int size, int low, int mid1, int mid2, int end)
 {
+    int left = low;
+    int right = mid2;
+    int mergeDex = low;
 
+    int merger[size];
+
+    while (left <= mid1 && right <= end)
+    {
+        if (array[left] < array[right])
+        {
+            merger[mergeDex] = array[left];
+        }
+        else
+        {
+            merger[mergeDex] = array[right];
+        }
+
+        left++;
+        right++;
+        mergeDex++;
+    }
+
+    while (left <= mid1)
+    {
+        merger[mergeDex] = array[left];
+
+        left++;
+        mergeDex++;
+    }
+
+    while (right <= mid1)
+    {
+        merger[mergeDex] = array[right];
+
+        right++;
+        mergeDex++;
+    }
 }
