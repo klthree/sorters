589c3df4c315742c55fefee72d65b62cb40a0272
Michael Buckner <bucknerml@gmail.com>
2017-09-01 01:53:44 +0000

    sorters headers

--- sorters.h
+++ sorters.h
@@ -2,12 +2,10 @@
  * Sorting function headers
  */
 
-#include "sorters.h"
-
-
 /**
  * Merge sort
  */
+
 void sort (int array[], int start, int end);
 void merge (int array[], int size, int low, int mid1, int mid2, int end);
 
@@ -15,10 +13,16 @@ void merge (int array[], int size, int low, int mid1, int mid2, int end);
  * Insertion sort
  */
 
+void insertionSort (int array[], int size)
+
 /**
  * Selection sort
  */
 
+void selectionSort (int array[], int size)
+
 /**
  * Bubble sort
  */
+
+void bubbleSort (int array[], int size)
